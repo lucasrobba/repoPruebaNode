21280b73a10d50fcb225ebab35eaa6d82ed25c01
lucasrobba <lucas_robba@hotmail.com>
2022-03-12 17:58:26 -0300

    segundo commit

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3002, ()=>{
-    console.log('Servidor funcionando 3002');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
+});
+
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
